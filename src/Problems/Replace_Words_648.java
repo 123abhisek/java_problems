436319610bb64a4533d1f170349a12c2aefc7301
123abhisek <abhishekjuvatkar2001@gmail.com>
2024-06-08 01:00:21 +0530

    update problems

--- src/Problems/Replace_Words_648.java
+++ src/Problems/Replace_Words_648.java
@@ -1,26 +1,59 @@
 package Problems;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
+class TrieNode {
+	Map<Character, TrieNode> children = new HashMap<>();
+	boolean isEndOfWord = false;
+}
+
+class Trie {
+	private TrieNode root = new TrieNode();
+
+	public void insert(String word) {
+		TrieNode node = root;
+		for (char c : word.toCharArray()) {
+			node = node.children.computeIfAbsent(c, k -> new TrieNode());
+		}
+		node.isEndOfWord = true;
+	}
+
+	public String searchShortestPrefix(String word) {
+		TrieNode node = root;
+		StringBuilder prefix = new StringBuilder();
+		for (char c : word.toCharArray()) {
+			if (!node.children.containsKey(c))
+				return word;
+			node = node.children.get(c);
+			prefix.append(c);
+			if (node.isEndOfWord)
+				return prefix.toString();
+		}
+		return word;
+	}
+}
 
 public class Replace_Words_648 {
 	public static String replaceWords(List<String> dictionary, String sentence) {
 
-		String[] strArr = sentence.split(" ");
-		int i = 0, j = 0;
-		while (i < dictionary.size()) {
-			
-			i++;
+		Trie trie = new Trie();
+		for (String root : dictionary) {
+			trie.insert(root);
 		}
 
-		return sentence;
+		String[] words = sentence.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			words[i] = trie.searchShortestPrefix(words[i]);
+		}
+
+		return String.join(" ", words);
 	}
 
 	public static void main(String[] args) {
 		List<String> dictionary = Arrays.asList("cat", "bat", "rat");
 		String sentence = "the cattle was rattled by the battery";
 
-		replaceWords(dictionary, sentence);
+		System.out.println(replaceWords(dictionary, sentence));
 
 	}
 
